5bce4fa6648022a01c383c8b920a18e64a0735f0
yurifeng <yuri_feng@outlook.com>
2018-09-17 14:33:42 +0800

    添加项目文件。

---
+++ SportsStore.Domain/Abstract/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using SportsStore.Domain.Entities;
+
+namespace SportsStore.Domain.Abstract
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> Products { get; }
+
+    }
+}
---
+++ SportsStore.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SportsStore.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SportsStore.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("41343aa8-5e28-4818-b1dc-b759975f1952")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
